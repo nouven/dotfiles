d6172ee2295fd7cd1c81f4f8c5bd4195e476a04d
nouven <doquynguyen20@gmail.com>
2024-06-04 10:29:50 +0700

    fix(nvim)/telescope

--- .config/nvim/lua/o9n/plugin/telescope.lua
+++ .config/nvim/lua/o9n/plugin/telescope.lua
@@ -4,7 +4,7 @@ function config()
   local transform_mod = require("telescope.actions.mt").transform_mod
 
   local trouble = require("trouble")
-  local trouble_telescope = require("trouble.providers.telescope")
+  local trouble_telescope = require("trouble.sources.telescope")
 
   -- or create your custom action
   local custom_actions = transform_mod({
